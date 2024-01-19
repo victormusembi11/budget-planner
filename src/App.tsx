3a117549a006a39777dd9582639d320ff0594cf0
victormusembi11 <victor.musembi11@gmail.com>
2024-01-20 00:17:23 +0300

    title

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <div className="App">Hello, World!</div>;
+  return (
+    <div className="container">
+      <h1 className="mt-3">My Budget planner</h1>
+    </div>
+  );
 }
