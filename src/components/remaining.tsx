be910d56fc9c155046f1798caac0e3a8951ad019
victormusembi11 <victor.musembi11@gmail.com>
2024-01-20 21:01:56 +0300

    calculate remaining after expense added

--- src/components/remaining.tsx
+++ src/components/remaining.tsx
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+
+import { AppContext, Expense } from "../context/app-context";
+
 export default function Remaining() {
+  const { expenses, budget } = useContext(AppContext) as {
+    expenses: Expense[];
+    budget: number;
+  };
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
   return (
     <div className="alert alert-success">
-      <span>Remaining: $1000</span>
+      <span>Remaining: ${budget - totalExpenses}</span>
     </div>
   );
 }
