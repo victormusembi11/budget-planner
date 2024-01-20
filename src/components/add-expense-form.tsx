f5685bf0ef151fed50e9b53ecbef024c44a3e240
victormusembi11 <victor.musembi11@gmail.com>
2024-01-20 14:41:32 +0300

    handle form to add expense

--- src/components/add-expense-form.tsx
+++ src/components/add-expense-form.tsx
@@ -1,17 +1,64 @@
+import React, { useState, useContext } from "react";
+
+import { AppContext } from "../context/app-context";
+
 export default function AddExpenseForm() {
+  const { dispatch } = useContext(AppContext) as {
+    dispatch: React.Dispatch<any>;
+  };
+  const [expense, setExpense] = useState({
+    name: "",
+    cost: 0,
+  });
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setExpense({
+      ...expense,
+      [event.target.id]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: {
+        name: expense.name,
+        cost: expense.cost,
+      },
+    });
+  }
+
   return (
     <form>
       <div className="row">
         <div className="col-sm">
           <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" id="name" required />
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="col-sm">
           <label htmlFor="cost">Cost</label>
-          <input type="text" className="form-control" id="cost" required />
+          <input
+            type="text"
+            className="form-control"
+            id="cost"
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="col-sm">
-          <button type="submit" className="btn btn-primary mt-3">
+          <button
+            type="submit"
+            className="btn btn-primary mt-3"
+            onClick={handleSubmit}
+          >
             Save
           </button>
         </div>
