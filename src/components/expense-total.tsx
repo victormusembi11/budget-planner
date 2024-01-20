4cb0324b41c95f4046cb7bf778b7bac972acbbee
victormusembi11 <victor.musembi11@gmail.com>
2024-01-20 21:10:00 +0300

    calculate and render total expense

--- src/components/expense-total.tsx
+++ src/components/expense-total.tsx
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+
+import { AppContext, Expense } from "../context/app-context";
+
 export default function ExpenseTotal() {
+  const { expenses } = useContext(AppContext) as { expenses: Expense[] };
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
   return (
     <div className="alert alert-primary">
-      <span>Spent so far: $1000</span>
+      <span>Spent so far: ${totalExpenses}</span>
     </div>
   );
 }
