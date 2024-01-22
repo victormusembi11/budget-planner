c6d70a0129fb017193e45ff3f3d29b0a637a164a
victormusembi11 <victor.musembi11@gmail.com>
2024-01-22 18:31:34 +0300

    make api call for expense data & store in reducer

--- src/components/expense-list.tsx
+++ src/components/expense-list.tsx
@@ -1,13 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
-import { AppContext, Expense } from "../context/app-context";
-
+import { AppContext } from "../context/app-context";
+import { Expense } from "../types/expense";
+import useFetch from "../hooks/use-fetch";
 import ExpenseItem from "./expense-item";
 
+import Loading from "./loading";
+import Error from "./error";
+
 export default function ExpenseList() {
-  const { expenses } = useContext(AppContext) as {
+  const { expenses, dispatch } = useContext(AppContext) as {
     expenses: Expense[];
+    dispatch: React.Dispatch<any>;
   };
+
+  const { data, loading, error } = useFetch("http://localhost:8000/expense/");
+
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: "SET_EXPENSES",
+        payload: data,
+      });
+    }
+  }, [data, dispatch]);
+
+  if (loading) return <Loading />;
+
+  if (error) return <Error />;
 
   return (
     <ul className="list-group">
