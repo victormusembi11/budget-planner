d60946ee1402aa7c655688dd2e504d9e7f658a32
victormusembi11 <victor.musembi11@gmail.com>
2024-01-22 18:30:56 +0300

    make api call for budget data & store in reducer

--- src/components/budget.tsx
+++ src/components/budget.tsx
@@ -1,8 +1,37 @@
-import { useContext } from "react";
-import { AppContext } from "../context/app-context";
+import { useContext, useEffect } from "react";
 
-export default function Budget() {
-  const { budget } = useContext(AppContext) as { budget: number };
+import { AppContext } from "../context/app-context";
+import useFetch from "../hooks/use-fetch";
+import Loading from "./loading";
+import Error from "./error";
+
+interface BudgetType {
+  id: number;
+  budget: number;
+}
+
+export default function BudgetCard() {
+  const { budget, dispatch } = useContext(AppContext) as {
+    budget: number;
+    dispatch: React.Dispatch<any>;
+  };
+
+  const { data, loading, error } = useFetch("http://localhost:8000/budget/1/");
+
+  const budgetData = data as unknown as BudgetType;
+
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: "SET_BUDGET",
+        payload: budgetData.budget,
+      });
+    }
+  }, [budgetData.budget, data, dispatch]);
+
+  if (loading) return <Loading />;
+
+  if (error) return <Error />;
 
   return (
     <div className="alert alert-secondary">
